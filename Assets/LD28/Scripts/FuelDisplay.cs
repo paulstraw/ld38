1448a2726882dd53ef35616b57122516b131733c
Paul Straw <paulstraw@paulstraw.com>
2017-04-22 22:07:15 -0700

    Auto-sizing for gravity and atmosphere markers.

---
+++ Assets/LD28/Scripts/FuelDisplay.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FuelDisplay : MonoBehaviour {
+	[SerializeField]
+	private RocketEngine engine;
+
+	private Text text;
+
+	void Awake() {
+		text = gameObject.GetComponent<Text>();
+	}
+
+	void Update() {
+		text.text = "Current fuel: " + engine.CurrentFuel;
+	}
+}
