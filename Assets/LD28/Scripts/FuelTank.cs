71950a3ce7f4814b04d369e55ee92c7939d98ccd
Paul Straw <paulstraw@paulstraw.com>
2017-04-23 04:51:01 -0700

    WIPWIPWIP

---
+++ Assets/LD28/Scripts/FuelTank.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuelTank : MonoBehaviour {
+	[SerializeField]
+	private Vector3 spin;
+	[SerializeField]
+	private float spinSpeed;
+
+	void Update() {
+		transform.Rotate(spin, spinSpeed * Time.deltaTime);
+	}
+}
