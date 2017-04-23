1524a9e77d9e9025462bb3a949ed3590d795e60f
Paul Straw <paulstraw@paulstraw.com>
2017-04-22 22:45:08 -0700

    Hey, finally not-terrible explosions!

--- Assets/LD28/Scripts/Rocket.cs
+++ Assets/LD28/Scripts/Rocket.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 using System.Linq;
 
 public class Rocket : MonoBehaviour {
+	[SerializeField]
+	private float explosionThreshold = 2.4f;
+
+	private RocketExploder exploder;
 	private Rigidbody rb;
 	private List<Transform> planets;
+	private Vector3 initialPosition;
+	private Quaternion initialRotation;
 
+	[HideInInspector]
 	public Transform closestPlanet;
+	[HideInInspector]
 	public float? distanceToClosestPlanet;
+	[HideInInspector]
 	public PlanetBody closestPlanetBody;
 
 	void Awake(){
+		exploder = gameObject.GetComponent<RocketExploder>();
 		rb = gameObject.GetComponent<Rigidbody>();
+		initialPosition = rb.position;
+		initialRotation = rb.rotation;
 
 		planets = GameObject.FindGameObjectsWithTag("planet").Select<GameObject, Transform>(
 			x => x.transform).ToList();
@@ -29,6 +41,24 @@ public class Rocket : MonoBehaviour {
 			rb.drag = closestPlanetBody.atmosphereDrag;
 		} else {
 			rb.drag = 0;
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		Rigidbody otherRb = other.gameObject.GetComponent<Rigidbody>();
+
+		if (otherRb == null) {
+			Debug.Log("No otherRb");
+			return;
+		}
+
+		float magnitude = rb.velocity.magnitude + rb.angularVelocity.magnitude + otherRb.velocity.magnitude + otherRb.angularVelocity.magnitude;
+
+		Debug.Log("Crash: " + magnitude);
+
+		if (magnitude >= explosionThreshold) {
+			Kill();
+			Invoke("Respawn", 4.2f);
 		}
 	}
 
@@ -56,5 +86,24 @@ public class Rocket : MonoBehaviour {
 			closestPlanetBody = null;
 			distanceToClosestPlanet = null;
 		}
+	}
+
+	private void Kill() {
+		exploder.Explode();
+		rb.detectCollisions = false;
+		rb.isKinematic = true;
+	}
+
+	void Respawn() {
+		rb.angularVelocity = Vector3.zero;
+		rb.velocity = Vector3.zero;
+
+		rb.rotation = initialRotation;
+		rb.position = initialPosition;
+
+		rb.detectCollisions = true;
+		rb.isKinematic = false;
+
+		exploder.Rebuild();
 	}
 }
---
+++ Assets/LD28/Scripts/RocketExploder.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PicaVoxel;
+
+public class RocketExploder : MonoBehaviour {
+	[SerializeField]
+	private GameObject explosionBlock;
+	[SerializeField]
+	private Volume volume;
+
+	public void Explode() {
+		for (int x = 0; x < volume.XSize; x++) {
+			for (int y = 0; y < volume.YSize; y++) {
+				for (int z = 0; z < volume.ZSize; z++) {
+					Voxel? vox = volume.GetVoxelAtArrayPosition(x, y, z);
+
+					if (y % 2 == 0 && vox.HasValue && vox.Value.State == VoxelState.Active) {
+						GameObject eb = Instantiate(explosionBlock);
+						eb.transform.position = volume.GetVoxelWorldPosition(x, y, z);
+						eb.GetComponent<Renderer>().material.color = vox.Value.Color;
+					}
+				}
+			}
+		}
+			
+		volume.SetFrame(1);
+	}
+
+	public void Rebuild() {
+		volume.SetFrame(0);
+	}
+}
